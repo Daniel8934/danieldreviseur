b0cd489c52264ab3a59fecea5974dc51f03a79f9
Daniel8934 <50085787+Daniel8934@users.noreply.github.com>
2020-12-21 21:04:39 -0500

    First Daniel commit

---
+++ js/js4.js
@@ -0,0 +1,27 @@
+// Code JS pour l'effet typewritter sur la page index
+var i = 0;
+var txt = 'Des mots plein la tête !';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+// Code pour le lien vers la page contact.html
+function contact() {
+    window.open("contact.html");
+}
+
+// Code de la barre de navigation
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
